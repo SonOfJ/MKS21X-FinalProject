725569f1697c3a7afffcb0aa34218b35ceba75aa
Jason Zheng <zzheng3@stuy.edu>
2019-01-09 11:33:46 -0500

    Established private variables for display class.

--- Display.java
+++ Display.java
@@ -1,6 +1,6 @@
 import javax.swing.JFrame;
-public class Screen {
-  private JFrame display;
+public class Display {
+  private JFrame frame;
   private String name;
   private int height;
   private int width;
