3b284032f5b26811c2a6ef724b98aae316e33bad
Jason Zheng <zzheng3@stuy.edu>
2019-01-09 11:36:02 -0500

    Added constructor for Display.java.

--- Display.java
+++ Display.java
@@ -4,4 +4,9 @@ public class Display {
   private String name;
   private int height;
   private int width;
+  public Display(String newName, int newHeight, int newWidth) {
+    name = newName;
+    height = newHeight;
+    width = newWidth;
+  }
 }
