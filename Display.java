671e29eee205ad87b83fbcabdb991a6854b614e8
Jason Zheng <zzheng3@stuy.edu>
2019-01-10 11:17:27 -0500

    Just finished with the create function for display.

--- Display.java
+++ Display.java
@@ -15,5 +15,7 @@ public class Display {
     frame.setSize(width, height);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Shuts down game when terminal closes.
     frame.setResizable(false); //No reason for the user to resize the window.
+    frame.setLocationRelativeTo(null); //It's better for the game to be displayed in the center.
+    frame.setVisible(true); //For some reason JFrames are not visible by default.
   }
 }
