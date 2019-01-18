4dbd40dc2065d3942da19502a6b22912a4247c43
Jason Zheng <zzheng3@stuy.edu>
2019-01-18 02:19:28 -0500

    Tidied up code due to errors with brackets.

--- Map.java
+++ Map.java
@@ -17,7 +17,6 @@ public class Map {
   private int seed;
   private Character tile[][];
   private String row[];
-
   public Map(int newSeed) {
     seed = newSeed;
     tile = new Character[50][50];
@@ -25,32 +24,28 @@ public class Map {
     if (seed == 1) {
       BufferedReader reader = new BufferedReader(new FileReader(World1.txt)); //Reader to read the text file that contains the map.
     }
-      int count = 0;
-      while (reader.readLine() != null) { //If there is still something to read
-        row[count] = reader.readline;
-      }
-
+    int count = 0;
+    while (reader.readLine() != null) { //If there is still something to read
+      row[count] = reader.readline;
+      count = count + 1;
+    }
     for(int i = 0; i < 50; i = i + 1) {
       for(int j = 0; j < 50; j = j + 1) {
         tile[i][j] = row[i].charAt(j);
       }
     }
   }
-}
-
   public String getRow(int rowNum) {
     return row[rowNum];
   }
-
-  public String print(){
+  public String print() {
     if (seed == 1){
       return "World 1 : 50x50 size";
     }
     return "NOT OPEN. ONLY ONE MAP CURRENTLY";
   }
-
   public boolean isWall(int y, int x){
-    //check is tile is a wall
+    //check is tile is a isWall
     return tile[y][x] != ' ';
   }
 }
