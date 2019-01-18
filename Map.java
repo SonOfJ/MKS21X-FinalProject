7cbc11785e7c8b841d750d608c247825b45f1f7b
Jason Zheng <zzheng3@stuy.edu>
2019-01-18 16:37:22 -0500

    Finally can read map from text file.

--- Map.java
+++ Map.java
@@ -1,4 +1,3 @@
-/*
 import com.googlecode.lanterna.terminal.Terminal.SGR;
 import com.googlecode.lanterna.TerminalFacade;
 import com.googlecode.lanterna.input.Key;
@@ -12,7 +11,6 @@ import com.googlecode.lanterna.input.InputDecoder;
 import com.googlecode.lanterna.input.InputProvider;
 import com.googlecode.lanterna.input.Key;
 import com.googlecode.lanterna.input.KeyMappingProfile;
-*/
 import java.util.*;
 import java.io.*;
 public class Map {
@@ -23,27 +21,21 @@ public class Map {
     seed = newSeed;
     tile = new Character[3][6];
     row = new String[3];
-
-    Scanner reader = new Scanner("World1.txt"); //Reader to read the text file that contains the map.
-    int count = 0;
-    while (reader.hasNext()) { //If there is still something to read
-      row[count] = reader.nextLine();
-      count = count + 1;
+    try {
+      Scanner reader = new Scanner(new File("World1.txt")); //Reader to read the text file that contains the map.
+      int count = 0;
+      while (reader.hasNext()) { //If there is still something to read
+        row[count] = reader.nextLine();
+        count = count + 1;
+      }
+    } catch (FileNotFoundException e) {
+      System.exit(1);
     }
-    /*
     for(int i = 0; i < 3; i = i + 1) {
       for(int j = 0; j < 6; j = j + 1) {
         tile[i][j] = row[i].charAt(j);
       }
     }
-    */
-  }
-  public String printArray() {
-    String kai = "";
-    for(int i = 0; i < row.length; i = i + 1) {
-      kai = kai + row[i];
-    }
-    return kai;
   }
   public String getRow(int rowNum) {
     return row[rowNum];
@@ -57,9 +49,5 @@ public class Map {
   public boolean isWall(int y, int x){
     //check is tile is a isWall
     return tile[y][x] != ' ';
-  }
-  public static void main(String[] args) {
-    Map map = new Map(1);
-    System.out.println(map.printArray());
   }
 }
