cae9256f9fb8548e46f37f1e1473dfdee0749cce
Jason Zheng <zzheng3@stuy.edu>
2019-01-18 11:47:50 -0500

    Work on text file,

--- Map.java
+++ Map.java
@@ -1,3 +1,4 @@
+/*
 import com.googlecode.lanterna.terminal.Terminal.SGR;
 import com.googlecode.lanterna.TerminalFacade;
 import com.googlecode.lanterna.input.Key;
@@ -11,6 +12,7 @@ import com.googlecode.lanterna.input.InputDecoder;
 import com.googlecode.lanterna.input.InputProvider;
 import com.googlecode.lanterna.input.Key;
 import com.googlecode.lanterna.input.KeyMappingProfile;
+*/
 import java.util.*;
 import java.io.*;
 public class Map {
@@ -19,19 +21,29 @@ public class Map {
   private String row[];
   public Map(int newSeed) {
     seed = newSeed;
-    tile = new Character[50][50];
-    row = new String[50];
+    tile = new Character[3][6];
+    row = new String[3];
+
     Scanner reader = new Scanner("World1.txt"); //Reader to read the text file that contains the map.
     int count = 0;
     while (reader.hasNext()) { //If there is still something to read
-      row[count] = reader.next();
+      row[count] = reader.nextLine();
       count = count + 1;
     }
+    /*
     for(int i = 0; i < 3; i = i + 1) {
       for(int j = 0; j < 6; j = j + 1) {
         tile[i][j] = row[i].charAt(j);
       }
     }
+    */
+  }
+  public String printArray() {
+    String kai = "";
+    for(int i = 0; i < row.length; i = i + 1) {
+      kai = kai + row[i];
+    }
+    return kai;
   }
   public String getRow(int rowNum) {
     return row[rowNum];
@@ -45,5 +57,9 @@ public class Map {
   public boolean isWall(int y, int x){
     //check is tile is a isWall
     return tile[y][x] != ' ';
+  }
+  public static void main(String[] args) {
+    Map map = new Map(1);
+    System.out.println(map.printArray());
   }
 }
